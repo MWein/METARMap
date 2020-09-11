75727d152a5caa11eaedc288832b42269a491866
Mike <michael.anthony.weinberg@monsanto.com>
2020-09-11 03:47:26 -0500

    Main function loops every 10 minutes

--- main.go
+++ main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
-	//"fmt"
+	"fmt"
+
+	"time"
 
 	"github.com/MWein/METARMap/encoding"
 	"github.com/MWein/METARMap/metar"
 )
 
 func main() {
-	var metars = metar.GetMetars()
-	var encodedMetar = encoding.EncodeMetars(metars, 0)
-	var reversedMetar = encoding.InverseEncodedMetars(encodedMetar)
+	for {
+		var metars = metar.GetMetars()
 
-	encoding.WriteToGPIO(reversedMetar)
+		if len(metars) != 0 {
+			var encodedMetar = encoding.EncodeMetars(metars, 0)
+			var reversedMetar = encoding.InverseEncodedMetars(encodedMetar)
+			encoding.WriteToGPIO(reversedMetar)
+		} else {
+			fmt.Println("Error. Trying again in 5 minutes")
+		}
+
+		// Wait for 10 minutes
+		time.Sleep(10 * time.Minute)
+	}
 }
